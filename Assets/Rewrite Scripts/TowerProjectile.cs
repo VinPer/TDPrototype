bcfa64921fe54e768cf4475751df0e422bfdcefc
Vinícius de Souza Pereira <vinpxk@gmail.com>
2019-07-01 18:44:15 -0300

    Add SciFi pack and SciFi barrels

---
+++ Assets/Rewrite Scripts/TowerProjectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerProjectile : TowerBase
+{
+    //private Magazine magazine;
+    public float fireRate = 1f;
+    public Transform target;
+    public int targettingStyle;
+
+
+    public override void UpgradeTower()
+    {
+        // upgrade logic
+    }
+}
