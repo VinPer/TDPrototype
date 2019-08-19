15bd2b37ed4e7f2a0474ff9c4f2c2742a43e668b
KevinYuki <kevin_iiyama@hotmail.com>
2019-08-19 10:51:31 -0300

    Remove camera rotation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,23 +1,29 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     private bool enableMovement = false;
 
+    Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
+
+    [Header("Movement")]
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
-
-    public float rotationSpeed = 100f;
-
+    //public float rotationSpeed = 100f;
     public float scrollSpeed = 5f;
+
+    [Header("Limits")]
     public float minY = 10f;
-    public float maxY = 130f;
+    public float maxY = 55f;
     public float minX = 5f;
     public float maxX = 70f;
     public float minZ = 0f;
     public float maxZ = 70;
 
-    Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
+    private float scroll;
+    private Vector3 pos;
 
     // Update is called once per frame
     void Update()
@@ -34,41 +40,43 @@ public class CameraController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.X)) enableMovement = !enableMovement;
         if (!enableMovement) return;
 
-        //Rotation
-        if (Input.GetKey("q"))
-        {
-            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.Self);
-        }
-        if (Input.GetKey("e"))
-        {
-            transform.Rotate(Vector3.down * rotationSpeed * Time.deltaTime, Space.Self);
-        }
+        ////Rotation
+        //if (Input.GetKey("q"))
+        //{
+        //    transform.Rotate(Vector3.down * rotationSpeed * Time.deltaTime, Space.Self);
+        //}
+        //if (Input.GetKey("e"))
+        //{
+        //    transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.Self);
+        //}
 
         //Translation
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
-            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+            //vector3.foward = new Vector3(0f,0f,1f)
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.Self);
         }
         if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
         {
-            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.Self);
         }
         if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
         {
-            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.Self);
         }
         if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
         {
-            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.Self);
         }
-
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-
-        Vector3 pos = transform.position;
+        //Zoom
+        scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos = transform.position;
         pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
         pos.x = Mathf.Clamp(pos.x, minX, maxX);
         pos.z = Mathf.Clamp(pos.z, minZ, maxZ);
         transform.position = pos;
+
+
     }
 }
