6209f291a5605ae7a37fe17147b35b2ae64d80a4
Raphael Hungria <rhja@uol.com.br>
2019-09-03 20:52:05 -0300

    Pre Black Hole Prep

---
+++ Assets/_RewriteEnemies/BlackHole/BlackHoleEnemy.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleEnemy : EnemyBase
+{
+    [Header("Black Hole Range Game Object")]
+    public GameObject blackHole;
+}
