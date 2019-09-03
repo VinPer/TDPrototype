5046a73ef1274a2a9c31fc1495418a4ce046b7ad
Caio de Araujo Morais <caioa.morais@gmail.com>
2019-09-02 22:32:26 -0300

    First Sketch - Main Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,9 @@ public class MainMenu : MonoBehaviour
     public string levelToLoad = "MainLevel";
 
     public SceneFader sceneFader;
-
+    [SerializeField]
+    private string nextSceneName;
+    private int nextLevelIndex;
     public void Play()
     {
         sceneFader.FadeTo(levelToLoad);
