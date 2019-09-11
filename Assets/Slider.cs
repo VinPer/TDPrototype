97902e660fa729aeac257ba644be5fd59fa82a1e
Caio de Araujo Morais <caioa.morais@gmail.com>
2019-09-10 22:31:26 -0300

    Slider ok (few bugs) + Start of Dialog System

--- Assets/Slider.cs
+++ Assets/Slider.cs
@@ -7,7 +7,10 @@ public class Slider : MonoBehaviour
 {
     public RectTransform panel; // hold the ScrollPanel
     public Button[] btn;
-    public RectTransform center; //Center to Compare
+    public RectTransform centerToCompare; //Center to Compare
+    public float scrollSpeed = 1.0f;
+
+    public Queue<string> senteces;
 
     private float[] distance; // btns distance to the center
     public float[] distRepostion;
@@ -32,10 +35,10 @@ public class Slider : MonoBehaviour
     {
         for (int i = 0; i < btn.Length; i++)
         {
-            distRepostion[i] = center.GetComponent<RectTransform>().position.x - btn[i].GetComponent<RectTransform>().position.x;
+            distRepostion[i] = centerToCompare.GetComponent<RectTransform>().position.x - btn[i].GetComponent<RectTransform>().position.x;
 
-            distance[i] = Mathf.Abs(center.GetComponent<RectTransform>().position.x - btn[i].GetComponent<RectTransform>().position.x);
-            Debug.Log(i+ ":" + distRepostion[i]);
+            distance[i] = Mathf.Abs(distRepostion[i]);
+            //Debug.Log(i+ ":" + distRepostion[i]);
             if (distRepostion[i] > -11)
             {
                 float curX = btn[i].GetComponent<RectTransform>().anchoredPosition.x;
@@ -46,6 +49,7 @@ public class Slider : MonoBehaviour
 
             }
         }
+
         float minDist = Mathf.Min(distance);
 
         for (int a = 0; a < btn.Length; a++)
@@ -53,18 +57,39 @@ public class Slider : MonoBehaviour
             if (minDist == distance[a])
             {
                 minBtnNum = a;
+                switch(btn[minBtnNum].name)
+                {
+                    case "Lv (1)":
+                        Debug.Log("Selected Level: " + btn[minBtnNum].name);
+                        break;
+                    case "Lv (2)":
+                        Debug.Log("Selected Level: " + btn[minBtnNum].name);
+                        break;
+                    case "Lv (3)":
+                        Debug.Log("Selected Level: " + btn[minBtnNum].name);
+                        break;
+                    case "Lv (4)":
+                        Debug.Log("Selected Level: " + btn[minBtnNum].name);
+                        break;
+                    case "Lv (5)":
+                        Debug.Log("Selected Level: " + btn[minBtnNum].name);
+                        break;
+                    case "Lv (6)":
+                        Debug.Log("Selected Level: " + btn[minBtnNum].name);
+                        break;
+                }
             }
         }
 
         if (!draggin)
         {
-            LerpToBtn(minBtnNum * -btnDist);
+            LerpToBtn(-btn[minBtnNum].GetComponent<RectTransform>().anchoredPosition.x);
         }
     }
 
-    void LerpToBtn(int pos)
+    void LerpToBtn(float pos)
     {
-        float newX = Mathf.Lerp(panel.anchoredPosition.x, pos, Time.deltaTime * 10f);
+        float newX = Mathf.Lerp(panel.anchoredPosition.x, pos, Time.deltaTime * scrollSpeed);
         Vector2 newPos = new Vector2(newX, panel.anchoredPosition.y);
 
         panel.anchoredPosition = newPos;
@@ -73,11 +98,13 @@ public class Slider : MonoBehaviour
     public void StartDrag()
     {
         draggin = true;
+        scrollSpeed = 2f;
     }
 
     public void EndDrag()
     {
         draggin = false;
+        scrollSpeed = 10f;
     }
 }
 
