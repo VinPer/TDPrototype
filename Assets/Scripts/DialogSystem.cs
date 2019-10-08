4c5bbd63ffcd49d1da8944afbfc695496f271300
Caio de Araujo Morais <caioa.morais@gmail.com>
2019-10-08 20:23:59 -0300

    SOCORRO! VAMOS MERGEAR!

--- Assets/Scripts/DialogSystem.cs
+++ Assets/Scripts/DialogSystem.cs
@@ -23,27 +23,17 @@ public class DialogSystem : MonoBehaviour
     {
         Debug.Log("Starting conversation");
 
-        //Como achei que seria:
         for(int i = 0; i < dialogue.senteces.Length; i++)
         {
             levelBtn.Add(i + 1, dialogue.senteces[i]);
         }
 
-        //Como tive que fazer
-        //levelBtn.Add(1, dialogue.senteces[0]);
-        //levelBtn.Add(2, dialogue.senteces[1]);
-        //levelBtn.Add(3, dialogue.senteces[2]);
-        //levelBtn.Add(4, dialogue.senteces[3]);
-        //levelBtn.Add(5, dialogue.senteces[4]);
-        //levelBtn.Add(6, dialogue.senteces[5]);
     }
 
     public void DisplayNextSentence(int numeroBtn)
     {
-        //Debug.Log("xubixubi:  " + levelBtn.Count);
 
         string sentence = levelBtn[numeroBtn];
-        //Debug.Log("tripa seca:  " + sentence);
 
         dialogText.SetText(sentence);
     }
