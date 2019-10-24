faf2243dc119b0e207f386885a38119dbf32c556
Caio de Araujo Morais <caioa.morais@gmail.com>
2019-10-24 20:57:57 -0300

    Ajustes UI

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+    public AudioSource audioSource;
+
+    public void SetVolume(float volume)
+    {
+
+        audioSource.volume = volume;
+    }
+
+    public void SetFullScreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
