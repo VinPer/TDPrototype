efd2e128b7c165a28d4ff8560c1a36956ee4c3af
KevinYuki <kevin_iiyama@hotmail.com>
2019-10-28 20:00:35 -0300

    Credits script

---
+++ Assets/RewriteScripts/Credits.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    public SceneFader sceneFader;
+    public string sceneToGo = "MainMenu";
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            sceneFader.FadeTo(sceneToGo);
+        }
+    }
+}
