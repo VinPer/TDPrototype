f809d2729a4e074b1db9619bf028227db9410ec1
KevinYuki <kevin_iiyama@hotmail.com>
2019-11-04 18:00:26 -0300

    Invisible enemy - visual response and some reference to negative armor

--- Assets/_RewriteEnemies/EnemyStatusBar.cs
+++ Assets/_RewriteEnemies/EnemyStatusBar.cs
@@ -4,13 +4,35 @@ public class EnemyStatusBar : MonoBehaviour
 {
     public Camera mainCamera;
 
+    private EnemyBase enemy;
+
+    private bool invisible = false;
+
     private void Start()
     {
         mainCamera = FindObjectOfType<Camera>();
+        enemy = GetComponentInParent<EnemyBase>();
     }
 
     void Update()
     {
         transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward, mainCamera.transform.rotation * Vector3.up);
+        if (enemy.GetInvisibleState())
+        {
+            if (!invisible)
+            {
+                print("teste");
+                GetComponentInChildren<CanvasGroup>().alpha = .2f;
+                invisible = true;
+            }
+        }
+        else
+        {
+            if (invisible)
+            {
+                GetComponentInChildren<CanvasGroup>().alpha = 1f;
+                invisible = false;
+            }
+        }
     }
 }
